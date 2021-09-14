15d443ffaa035cd3c7613a21daa2037e45041ba2
rublesKelly <rkellymkk@gmail.com>
2021-09-14 11:11:17 +0100

    V1 004

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+//Imports 
 import './App.css';
+import React,{ useState, useEffect } from "react";
+import axios  from "axios";
+//Component Imports
+import DashBoard from './Components/DashBoard';
+import SearchBar from './Components/SearchBar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* This is a temporary search bar till the side bar is in */}
+      <SearchBar/> 
+      <DashBoard/>
     </div>
   );
 }
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import React, {useState} from 'react'
+import Web3 from "web3-eth";
+
+
+function SearchBar(props) {
+
+//Set State
+const [ethAddress, setEthAddress] = useState('')
+const [URL, setURL] = useState('')
+
+// Conditional logic to run something if eth address is valid 
+// 
+
+//onChangeHandler is this needed at all?
+const onChangeHandler = (e) => {
+    console.log(e.target.value);
+    e.stopPropagation();
+
+}
+
+//onSubmitHandler
+const onSubmitHandler = (e) => {
+    e.preventDefault()
+    console.log(e.target.value);
+    // Web3.utils.isAddress(e.target.value) ? setEthAddress(e.target.value) : alert('not a valid address')
+}
+
+// //UseEffect to update dashboard
+
+
+    return (
+        <div id='search-bar'>
+            <form onSubmit={onSubmitHandler}>
+                <label>Enter an Eth Address</label>
+                <input type='input'
+                       id='search-bar'
+                       placeholder='0x..5h5d'
+                       onChange={onChangeHandler}
+                ></input>  
+            </form>
+            
+        </div>
+    )
+}
+
+export default SearchBar
